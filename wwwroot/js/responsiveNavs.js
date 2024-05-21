2dcec950e7dfde20658e37041cb402e208ba1906
Farida Mukhtar <farida.moukhtar03@eng-st.cu.edu.eg>
2024-05-21 15:26:14 +0300

    header apply

---
+++ wwwroot/js/responsiveNavs.js
@@ -0,0 +1,11 @@
+﻿var navContainer = document.getElementById("navbar-selections");
+
+var navButtons = navContainer.getElementsByClassName("nav-link");
+
+for (var i = 0; i < navButtons.length; i++) {
+    navButtons[i].addEventListener("click", function () {
+        var current = document.getElementsByClassName("active");
+        current[0].className = current[0].className.replace(" active", "");
+        this.className += " active";
+    });
+}
